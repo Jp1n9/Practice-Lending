cd31429c4ca41961befb24a144944f28f5e1139b
beomjoon <beomjoon.kim@dream-academy.io>
2022-10-06 20:46:30 +0900

    init

---
+++ src/Lending.sol
@@ -0,0 +1,253 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+import "forge-std/Test.sol";
+
+import "./DreamOracle.sol";
+import "openzeppelin-contracts/token/ERC20/utils/SafeERC20.sol";
+
+contract Lending {
+
+    using SafeERC20 for IERC20;
+    // USDC
+    address private _token;
+    // Ether 
+    address private _ether = address(0x1234);
+
+    uint256 constant private _ltv = 500;
+    uint256 constant private _threshold = 750; 
+    uint256 constant private _bonus = 50;
+    uint256 constant private _BASE_POINT = 1000;
+     
+
+    DreamOracle private _oracle; 
+
+    struct sCollateral {
+        uint256 ethAmount; // ETH
+        uint256 time; // Block timestamp
+        uint256 borrowableAmount; // borrowable amount
+        uint256 thresholdAmount; // threshold amount
+        uint256 limitThresholdAmount;
+        uint256 borrowedAmount; // borrowed amount
+    }
+
+    struct sDeposit {
+        uint256 usdcAmount; // USDC
+        uint256 time; // Block timestamp
+    }
+
+
+
+    mapping(address => sCollateral) public  borrowers;
+    mapping(address => sDeposit) public depositors;
+
+    address[] public  borrowerAddressList;
+
+    event DepositUSDC(address from , uint256 amount);
+    event DepositETHER(address from , uint256 amount);
+
+    event DepositorInfo(address depositorAddr, uint256 usdcAmount, uint256 time);
+    event BorrowerInfo(address borrowerAddr, uint256 ethAmount, uint256 time, uint256 borrowableAmount , uint256 threshold, uint256 borrowedAmount);
+
+
+    constructor(address token,address oracle) {
+        _token = token;
+        _oracle = DreamOracle(oracle);
+    }
+
+    function deposit(address tokenAddress, uint256 amount) external  payable{
+        require(tokenAddress == address(0) || tokenAddress == _token,"only USDC,ETHER is supported");
+        
+        // Collateral
+        if(msg.value > 0)  {
+            uint256 ethToUsdc = msg.value * _oracle.getPrice(_ether) / 10 ** 18;
+
+            sCollateral memory collateral=  sCollateral({
+                ethAmount : msg.value,
+                time : 0,
+                borrowableAmount : ethToUsdc * _ltv / _BASE_POINT,
+                thresholdAmount : ethToUsdc * _threshold / _BASE_POINT,
+                limitThresholdAmount : ethToUsdc * _ltv / _BASE_POINT,
+                borrowedAmount : 0
+            });
+
+            borrowers[msg.sender] = collateral;
+
+            emit DepositETHER(msg.sender, msg.value);
+            emit BorrowerInfo(msg.sender, borrowers[msg.sender].ethAmount, borrowers[msg.sender].time,borrowers[msg.sender].borrowableAmount, borrowers[msg.sender].thresholdAmount,borrowers[msg.sender].borrowedAmount);
+        }
+        // USDC
+        else if(tokenAddress == _token){
+            require(amount > 0 , "Amount must be greater than 0 ");
+            sDeposit memory deposit = sDeposit( {
+                usdcAmount : amount,
+                time : block.timestamp
+            });
+
+            depositors[msg.sender] = deposit;
+            IERC20(tokenAddress).safeTransferFrom(msg.sender,address(this),amount);
+
+            emit DepositUSDC(msg.sender, amount);
+            emit DepositorInfo(msg.sender, depositors[msg.sender].usdcAmount,depositors[msg.sender].time);
+
+        }
+    }
+
+
+    function borrow(address tokenAddress, uint256 amount) external {
+        require(tokenAddress == _token, "Only USDC is supported");
+        sCollateral memory collateral = borrowers[msg.sender];
+        require(collateral.ethAmount > 0, "Input your collateral first");
+        require(collateral.borrowableAmount >= amount, "Amount greater than the borrowable amount" );
+        
+       collateral.borrowedAmount += amount;
+       collateral.borrowableAmount -= amount; 
+       collateral.time = block.timestamp;
+       borrowers[msg.sender] = collateral;
+       borrowerAddressList.push(msg.sender);
+       IERC20(tokenAddress).safeTransfer(msg.sender,amount);
+    
+        emit BorrowerInfo(msg.sender, borrowers[msg.sender].ethAmount, borrowers[msg.sender].time,borrowers[msg.sender].borrowableAmount, borrowers[msg.sender].thresholdAmount,borrowers[msg.sender].borrowedAmount);
+
+
+    }
+
+
+    function repay(address tokenAddress, uint256 amount) external {
+      require(tokenAddress == _token,"Only USDC is supported");  
+
+      sCollateral memory collateral = borrowers[msg.sender];
+      uint256 borrowedPeriod = (block.timestamp - collateral.time) / 1 days; // 빌린 기간
+
+      uint256 currentBorrowedAmount = calcInterest(collateral.borrowedAmount,borrowedPeriod);
+      console.log("current" ,currentBorrowedAmount);
+      uint256 fee = calcInterest(collateral.borrowedAmount,borrowedPeriod) - collateral.borrowedAmount; // 수수료 
+      uint256 originAmount = amount - fee;
+      // 빌린 금액 보다 적은 금액을 상환 했을 때
+      if(currentBorrowedAmount > amount) {
+        collateral.borrowedAmount = currentBorrowedAmount - amount;
+        collateral.borrowableAmount += originAmount;
+        collateral.time = block.timestamp;
+      }
+      // 빌린 금액을 다 상환 했을 때
+      else {
+        collateral.borrowedAmount = 0;
+        collateral.time = 0;
+        collateral.borrowableAmount += collateral.borrowedAmount;
+        collateral.thresholdAmount = 0;
+      }
+      IERC20(tokenAddress).safeTransferFrom(msg.sender,address(this),amount);
+      borrowers[msg.sender] = collateral;
+      emit BorrowerInfo(msg.sender, borrowers[msg.sender].ethAmount, borrowers[msg.sender].time,borrowers[msg.sender].borrowableAmount, borrowers[msg.sender].thresholdAmount,borrowers[msg.sender].borrowedAmount);
+      
+    }
+
+
+    function withdraw(address tokenAddress, uint256 amount) external payable{
+        require(tokenAddress == _token || tokenAddress == address(0) ,"Only USDC is supported");
+        // Depositor
+        if(tokenAddress == _token) {
+            sDeposit memory deposit = depositors[msg.sender];
+            require(deposit.usdcAmount > 0 ,"you must be deposit usdc");
+
+            uint256 depositedPeriod = (block.timestamp - deposit.time) / 1 days;
+            deposit.usdcAmount = calcInterest(deposit.usdcAmount,depositedPeriod); // 0.1% 복리 이자
+            if(deposit.usdcAmount == amount) {
+                delete depositors[msg.sender];
+            }
+            else {
+                deposit.time = block.timestamp;
+                deposit.usdcAmount -= amount;
+                depositors[msg.sender] = deposit;
+            }
+            IERC20(_token).safeTransfer(msg.sender,amount) ;
+            emit DepositorInfo(msg.sender, depositors[msg.sender].usdcAmount,depositors[msg.sender].time);
+
+
+        }
+        // commplete redeem 
+        else {
+            sCollateral memory collateral = borrowers[msg.sender];
+            require(collateral.borrowedAmount ==0, "You didnt redeem complete");
+            uint256 balance = collateral.ethAmount;
+            delete borrowers[msg.sender];
+
+            // send Ether
+            (bool success,) = payable(msg.sender).call{
+                value : balance
+            }(""); 
+            require(success,"Failed to send ether");  
+            emit BorrowerInfo(msg.sender, borrowers[msg.sender].ethAmount, borrowers[msg.sender].time,borrowers[msg.sender].borrowableAmount, borrowers[msg.sender].thresholdAmount,borrowers[msg.sender].borrowedAmount);
+
+        }
+
+
+    }
+
+
+    function liquidate(address user, address tokenAddress, uint256 amount) external payable{
+        require(tokenAddress == _token ,"Only USDC is supported");
+        require(borrowers[user].ethAmount > 0 , "this user is not applicable" );
+
+        //Threshold check
+        sCollateral memory collateral = borrowers[user];
+        uint256 ethToUsdc = collateral.ethAmount * _oracle.getPrice(_ether) / 10 ** 18;
+        require(ethToUsdc <= collateral.thresholdAmount ,"Ether price is greater than the threshold");
+        uint256 liquidatorBonus;
+        uint256 liquidatorAmountEther;    
+
+        // 75%  ~ 51% 이면 빌린양의 1/2 까지만 상환 가능
+        if(ethToUsdc > collateral.limitThresholdAmount) {
+            require(collateral.borrowedAmount <= amount * 2, "The amount that can be repaid has been exceeded");
+            //Bonus + amount -> ether
+            liquidatorBonus = amount * _bonus / _BASE_POINT;
+            liquidatorAmountEther = ((amount+liquidatorBonus) * 10 ** 18 / _oracle.getPrice(_ether));
+
+            uint256 borrowedPeriod = (block.timestamp - collateral.time) / 1 days; 
+            collateral.borrowedAmount = calcInterest(collateral.borrowedAmount,borrowedPeriod);
+
+            collateral.ethAmount -= liquidatorAmountEther;
+            collateral.borrowedAmount -= amount;
+            collateral.time = block.timestamp;
+            collateral.borrowableAmount= collateral.borrowableAmount + amount >= collateral.limitThresholdAmount  ? collateral.limitThresholdAmount :  collateral.borrowableAmount + amount;
+            borrowers[user] = collateral;
+            
+
+        }
+        //  50% 아래 면 45% 까지 상환 가능(5%는 수수료)
+        else {
+            if( amount >= (collateral.limitThresholdAmount*2) * 450 / 1000 ) {
+                liquidatorAmountEther = collateral.ethAmount;
+                delete borrowers[user];
+            }
+            else {
+                liquidatorAmountEther = amount * 10 ** 18 /_oracle.getPrice(_ether);
+                liquidatorBonus = liquidatorAmountEther * _bonus / _BASE_POINT;
+                liquidatorAmountEther += liquidatorBonus;
+                collateral.ethAmount -= liquidatorAmountEther;
+                collateral.borrowedAmount -= amount;
+                collateral.time = block.timestamp;
+                collateral.borrowableAmount= collateral.borrowableAmount + amount >= collateral.limitThresholdAmount  ? collateral.limitThresholdAmount :  collateral.borrowableAmount + amount;
+                borrowers[user] = collateral;
+            }
+
+        }
+
+
+        IERC20(_token).safeTransferFrom(msg.sender,address(this),amount);
+
+        bool success = payable(msg.sender).send(liquidatorAmountEther);
+        require(success,"Failed to send Ether");
+
+        {
+        sCollateral memory borrower = borrowers[user];
+        console.log(borrower.ethAmount);
+        emit BorrowerInfo(user, borrower.ethAmount, borrower.time,borrower.borrowableAmount, borrower.thresholdAmount,borrower.borrowedAmount);
+        }
+        
+    }
+
+    function calcInterest(uint256 amount ,uint256 n) private returns(uint256) {
+        return (amount* 1001 ** n ) / (1000 ** n);
+     }
+
+}
---
+++ test/Lending.t.sol
@@ -0,0 +1,417 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/Lending.sol";
+import "../src/DreamOracle.sol";
+import "openzeppelin-contracts/token/ERC20/ERC20.sol";
+
+
+contract USDC is ERC20 {
+    constructor(string memory tokenName) ERC20(tokenName,tokenName) {}
+
+
+    function mint(address to,uint256 amount) public {
+        _mint(to,amount);
+    }
+}
+
+
+
+contract CounterTest is Test {
+    Lending lending;
+    DreamOracle oracle;
+    USDC usdc;
+    address eth = address(0x1234);
+
+
+    address depositor1;
+    address borrower1;
+
+    function setUp() public {
+        usdc = new USDC("USDC");
+        oracle = new DreamOracle();
+        lending = new Lending(address(usdc),address(oracle));
+
+        oracle.setPrice(eth,1400 * 10 ** 6);
+
+        depositor1 = address(0x11);
+        borrower1 = address(0x10);
+
+    }
+    function testDepositUSDC1() public {
+        usdc.mint(depositor1,100 * 10 ** 6);
+
+        vm.startPrank(depositor1);
+        usdc.approve(address(lending),100 * 10 **6);
+        lending.deposit(address(usdc),100 * 10 **6);
+
+
+        assertEq(usdc.balanceOf(address(lending)), 100 * 10 ** 6, "Error USDC deposit");
+        vm.stopPrank();
+
+    }
+
+    function testDepositCollateral1() public {
+        vm.deal(borrower1,10 ether);
+        vm.prank(borrower1);
+        lending.deposit{value : 10 ether}(address(0),0);
+
+        assertEq(address(lending).balance , 10 ether,"Error Ether deposit");
+    }
+
+    function testBorrow1() public {
+        uint256 amount = 10 ether;
+        usdc.mint(depositor1,7000 * 10 ** 6);
+
+
+        // deposit
+        vm.startPrank(depositor1);
+        usdc.approve(address(lending),7000 * 10 **6);
+        lending.deposit(address(usdc),7000 *10 **6);
+        vm.stopPrank();
+        // borrow
+        vm.deal(borrower1, 10 ether);
+        vm.startPrank(borrower1);
+        lending.deposit{value : amount }(address(0),0);
+
+        assertEq(address(lending).balance , 10 ether , "Check collateral deposit" );
+
+        lending.borrow(address(usdc), 7000 * 10 **6 );
+        assertEq(usdc.balanceOf(address(borrower1)),7000 * 10 **6);
+
+        vm.expectRevert("Amount greater than the borrowable amount");
+        lending.borrow(address(usdc), 7000 * 10 **6 );
+        vm.stopPrank();
+
+    }
+    function testBorrow2() public {
+        uint256 amount = 10 ether;
+        usdc.mint(depositor1,7000 * 10 ** 6);
+
+
+        // deposit
+        vm.startPrank(depositor1);
+        usdc.approve(address(lending),7000 * 10 **6);
+        lending.deposit(address(usdc),7000 *10 **6);
+        vm.stopPrank();
+        // borrow
+        vm.deal(borrower1, 10 ether);
+        vm.startPrank(borrower1);
+        
+        // 담보를 예치  안했을 경우
+        //lending.deposit{value : amount }(address(0),0);
+
+        vm.expectRevert("Input your collateral first");
+        lending.borrow(address(usdc), 7000 * 10 **6 );
+        vm.stopPrank();
+
+    }
+
+    // 상환을 완료하였을 때
+    function testRepay1() public {
+       uint256 amount = 10 ether;
+        usdc.mint(depositor1,7000 * 10 ** 6);
+      usdc.mint(borrower1,70315841);
+
+        // deposi때
+        vm.startPrank(depositor1);
+        usdc.approve(address(lending),7000 * 10 **6);
+        lending.deposit(address(usdc),7000 *10 **6);
+        vm.stopPrank();
+
+        // borrow
+        vm.deal(borrower1, 10 ether);
+        vm.startPrank(borrower1);
+        lending.deposit{value : amount }(address(0),0);
+
+        assertEq(address(lending).balance , 10 ether , "Check collateral deposit" );
+
+        lending.borrow(address(usdc), 7000 * 10 **6 );
+
+        
+        // repay
+        // 10일이 지난 후 (복리)
+        console.log(usdc.balanceOf(borrower1));
+        usdc.approve(address(lending),7070315841);
+        vm.warp(block.timestamp + 10 days);
+        lending.repay(address(usdc),7070315841 );
+        
+        lending.withdraw(address(0),0);
+        assertEq(borrower1.balance,10 ether,"Check repay function, Collateral did not come in properly");
+        vm.stopPrank();
+    }
+
+    // 부분 상환
+    function testRepay2() public {
+       uint256 amount = 10 ether;
+        usdc.mint(depositor1,7000 * 10 ** 6);
+        usdc.mint(borrower1,70315841);
+
+        // deposit
+        vm.startPrank(depositor1);
+        usdc.approve(address(lending),7000 * 10 **6);
+        lending.deposit(address(usdc),7000 *10 **6);
+        vm.stopPrank();
+
+        // borrow
+        vm.deal(borrower1, 10 ether);
+        vm.startPrank(borrower1);
+        lending.deposit{value : amount }(address(0),0);
+
+        assertEq(address(lending).balance , 10 ether , "Check collateral deposit" );
+
+        lending.borrow(address(usdc), 7000 * 10 **6 );
+
+        
+        // repay
+        // 10일이 지난 후 (복리)
+        console.log(usdc.balanceOf(borrower1));
+        usdc.approve(address(lending),7070315841);
+        vm.warp(block.timestamp + 10 days);
+        lending.repay(address(usdc),4000000000 );
+        assertEq(usdc.balanceOf(borrower1),7070315841-4000000000,"Did not repay");
+        vm.stopPrank();
+    }
+    
+    // Deposit한 USDC 전체 인출
+    function testWithdraw1() public {
+        usdc.mint(depositor1,100 * 10 ** 6);
+
+        vm.startPrank(depositor1);
+        usdc.approve(address(lending),100 * 10 **6);
+        lending.deposit(address(usdc),100 * 10 **6);
+        assertEq(usdc.balanceOf(address(lending)), 100 * 10 ** 6, "Error USDC deposit");
+
+        lending.withdraw(address(usdc), 100 * 10 **6);
+        assertEq(usdc.balanceOf(depositor1),100 * 10 **6);        
+        vm.stopPrank();  
+
+    }
+    // Deposit한 USDC 부분 인출
+    function testWithdraw2() public {
+        usdc.mint(depositor1,100 * 10 ** 6);
+
+        vm.startPrank(depositor1);
+        usdc.approve(address(lending),100 * 10 **6);
+        lending.deposit(address(usdc),100 * 10 **6);
+        assertEq(usdc.balanceOf(address(lending)), 100 * 10 ** 6, "Error USDC deposit");
+
+        lending.withdraw(address(usdc), 50* 10 **6);
+        assertEq(usdc.balanceOf(depositor1),50 * 10 **6);        
+        vm.stopPrank();  
+
+    }
+    // 5일 후 복리 계산출여 인출
+    function testWithdraw3() public {
+        // 35070070 5일 복리
+        usdc.mint(depositor1,7000 * 10 ** 6);
+        vm.deal(borrower1,10 ether);
+        usdc.mint(borrower1,35070070);
+
+        // deposit
+        vm.startPrank(depositor1);
+        usdc.approve(address(lending),7000 * 10 **6);
+        console.log("before deposit" , usdc.balanceOf(depositor1));
+        lending.deposit(address(usdc),7000 *10 **6);
+        vm.stopPrank();
+
+        // borrow
+        vm.startPrank(borrower1);
+        usdc.approve(address(lending),100 * 10 **6);
+        lending.deposit{value : 10 ether }(address(0),0);
+        lending.borrow(address(usdc), 7000 *10 **6 );
+        
+        // repay
+        usdc.approve(address(lending),	7035070070);
+        vm.warp(block.timestamp + 5 days);
+        lending.repay(address(usdc),  7035070070);
+        assertEq(usdc.balanceOf(address(lending)), 7035070070,"Check repay");       
+        vm.stopPrank();    
+
+        vm.startPrank(depositor1);
+        lending.withdraw(address(usdc), 7035070070 );
+        assertEq(usdc.balanceOf(depositor1),7035070070, "dont receive welfare" );
+
+    }
+   function testWithdraw4() public {
+        // 35070070 5일 복리
+        usdc.mint(depositor1,7000 * 10 ** 6);
+        vm.deal(borrower1,10 ether);
+        usdc.mint(borrower1,35070070);
+
+        // deposit
+        vm.startPrank(depositor1);
+        usdc.approve(address(lending),7000 * 10 **6);
+        console.log("before deposit" , usdc.balanceOf(depositor1));
+        lending.deposit(address(usdc),7000 *10 **6);
+        vm.stopPrank();
+
+        // borrow
+        vm.startPrank(borrower1);
+        usdc.approve(address(lending),100 * 10 **6);
+        lending.deposit{value : 10 ether }(address(0),0);
+        lending.borrow(address(usdc), 7000 *10 **6 );
+        
+        // repay
+        usdc.approve(address(lending),	7035070070);
+        vm.warp(block.timestamp + 5 days);
+        lending.repay(address(usdc),  7035070070);
+        assertEq(usdc.balanceOf(address(lending)), 7035070070,"Check repay");       
+        vm.stopPrank();    
+
+        vm.startPrank(depositor1);
+        lending.withdraw(address(usdc), 3000 * 10 **6 );
+        
+
+    }
+
+    function testLiquidate1() public {
+        // 35070070 5일 복리
+        usdc.mint(depositor1,7000 * 10 ** 6);
+        vm.deal(borrower1,10 ether);
+        usdc.mint(borrower1,35070070);
+
+        // deposit
+        vm.startPrank(depositor1);
+        usdc.approve(address(lending),7000 * 10 **6);
+        lending.deposit(address(usdc),7000 *10 **6);
+        vm.stopPrank();
+
+        // borrow
+        vm.startPrank(borrower1);
+        usdc.approve(address(lending),100 * 10 **6);
+        lending.deposit{value : 10 ether }(address(0),0);
+        lending.borrow(address(usdc), 7000 *10 **6 ); 
+        vm.stopPrank();
+
+        address liquidator1 = address(0x90);
+        usdc.mint(liquidator1,7000 * 10 ** 6);
+        // Threshold 75%
+        oracle.setPrice(eth,1000* 10 ** 6);
+
+        vm.startPrank(liquidator1);
+        // vm.warp(block.timestamp + 5 days);///
+        usdc.approve(address(lending),7000 * 10 ** 6);
+        vm.warp(block.timestamp + 5 days);
+        lending.liquidate(borrower1, address(usdc),7000 * 10 **6);
+        assertEq(liquidator1.balance, 7350000000000000000 );
+        assertEq(address(lending).balance,10 ether - 7350000000000000000);
+        vm.stopPrank();
+    }
+
+    function testLiquidate2() public {
+        // 35070070 5일 복리
+        usdc.mint(depositor1,7000 * 10 ** 6);
+        vm.deal(borrower1,10 ether);
+        usdc.mint(borrower1,35070070);
+
+        // deposit
+        vm.startPrank(depositor1);
+        usdc.approve(address(lending),7000 * 10 **6);
+        lending.deposit(address(usdc),7000 *10 **6);
+        vm.stopPrank();
+
+        // borrow
+        vm.startPrank(borrower1);
+        usdc.approve(address(lending),100 * 10 **6);
+        lending.deposit{value : 10 ether }(address(0),0);
+        lending.borrow(address(usdc), 7000 *10 **6 ); 
+        vm.stopPrank();
+
+        address liquidator1 = address(0x90);
+        usdc.mint(liquidator1,7000 * 10 ** 6);
+        // Threshold 75%
+        oracle.setPrice(eth,1000 * 10 ** 6);
+
+        vm.startPrank(liquidator1);
+        // vm.warp(block.timestamp + 5 days);///
+        usdc.approve(address(lending),7000 * 10 ** 6);
+        vm.warp(block.timestamp + 5 days);
+        lending.liquidate(borrower1, address(usdc),7000 * 10 **6);
+        assertEq(liquidator1.balance, 7350000000000000000 );
+        vm.stopPrank();
+
+
+        vm.startPrank(borrower1);
+
+        // 나머지 금액을 다 갚으면 남아있는 담보를 모두 상환 
+        uint256 lendingEther = address(lending).balance;
+        lending.repay(address(usdc), 35070070);
+        lending.withdraw(address(0),0);
+        assertEq(borrower1.balance,lendingEther, "dont repay remainder" );
+    }
+
+
+    function testLiquidate3() public {
+        // 35070070 5일 복리
+        usdc.mint(depositor1,7000 * 10 ** 6);
+        vm.deal(borrower1,10 ether);
+        usdc.mint(borrower1,35070070);
+
+        // deposit
+        vm.startPrank(depositor1);
+        usdc.approve(address(lending),7000 * 10 **6);
+        lending.deposit(address(usdc),7000 *10 **6);
+        vm.stopPrank();
+
+        // borrow
+        vm.startPrank(borrower1);
+        usdc.approve(address(lending),100 * 10 **6);
+        lending.deposit{value : 10 ether }(address(0),0);
+        lending.borrow(address(usdc), 7000 *10 **6 ); 
+        vm.stopPrank();
+
+        address liquidator1 = address(0x90);
+        usdc.mint(liquidator1,7000 * 10 ** 6);
+
+        oracle.setPrice(eth,300* 10 ** 6);
+        uint256 getPrice = oracle.getPrice(eth);
+
+        vm.startPrank(liquidator1);
+        // vm.warp(block.timestamp + 5 days);///
+        usdc.approve(address(lending),7000 * 10 ** 6);
+        vm.warp(block.timestamp + 5 days);
+        //이더의 가치가50%이하로 떨어지면 45% 까지 청산시켜주는 사람에게  바로 청산
+        lending.liquidate(borrower1, address(usdc),6600 * 10 **6);
+        assertEq(liquidator1.balance,10 ether);
+        console.log(liquidator1.balance);
+        vm.stopPrank();
+
+    }
+    function testLiquidate4() public {
+        // 35070070 5일 복리
+        usdc.mint(depositor1,7000 * 10 ** 6);
+        vm.deal(borrower1,10 ether);
+        usdc.mint(borrower1,35070070);
+
+        // deposit
+        vm.startPrank(depositor1);
+        usdc.approve(address(lending),7000 * 10 **6);
+        lending.deposit(address(usdc),7000 *10 **6);
+        vm.stopPrank();
+
+        // borrow
+        vm.startPrank(borrower1);
+        usdc.approve(address(lending),100 * 10 **6);
+        lending.deposit{value : 10 ether }(address(0),0);
+        lending.borrow(address(usdc), 7000 *10 **6 ); 
+        vm.stopPrank();
+
+        address liquidator1 = address(0x90);
+        usdc.mint(liquidator1,7000 * 10 ** 6);
+
+        oracle.setPrice(eth,400* 10 ** 6);
+        uint256 getPrice = oracle.getPrice(eth);
+
+        vm.startPrank(liquidator1);
+        // vm.warp(block.timestamp + 5 days);///
+        usdc.approve(address(lending),7000 * 10 ** 6);
+        vm.warp(block.timestamp + 5 days);
+        //이더의 가치가50%이하로 떨어지면 45% 까지 청산시켜주는 사람에게  바로 청산
+        lending.liquidate(borrower1, address(usdc),2000 * 10 **6);
+        assertEq(liquidator1.balance, 5.25 ether);
+        vm.stopPrank();
+
+    }
+}
